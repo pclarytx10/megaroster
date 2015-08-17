56ed2025057ff675b94b60ada1d750eafa3265f5
Patrick Clary <patrickm@theclarys.org>
2015-08-17 16:44:57 -0400

    Add students to list. Clear and focus on entry.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,8 +1,26 @@
 var MegaRoster = function() {
-  this.init = function() {
-    alert('What?')
+  this.veggies = ['cabbage', 'brocolli'];
 
-  }
+  this.init = function() {
+    var self = this;
+    this.students = [];
+
+    $('#new_student_form').on('submit',function(ev){
+      ev.preventDefault();
+      var student_name = $(this.student_name).val();
+      //Push the student name into the students array
+      self.students.push(student_name);
+
+      //Use the console to prove that the array has a new student
+      //console.log(self.students);
+
+      //Add the student name to a new list item in the <ol>
+      $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+      $(this.student_name).val('');
+      $(this.student_name).focus();
+    });
+  };
+
 };
 
 var roster = new MegaRoster();
